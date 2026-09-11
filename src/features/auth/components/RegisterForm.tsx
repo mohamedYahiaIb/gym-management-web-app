9414e996f9de525bb0b4e3a0e696d0f14bdb6c1b
mohamedYahiaIb <ibriz.yahia@univ-constantine2.dz>
2026-09-11 18:51:52 +0100

    adding a register form

---
+++ src/features/auth/components/RegisterForm.tsx
@@ -0,0 +1,13 @@
+import Input from "../../../components/ui/Input";
+import Button from "../../../components/ui/Button";
+import { useState } from "react";
+
+
+function RegisterForm() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
