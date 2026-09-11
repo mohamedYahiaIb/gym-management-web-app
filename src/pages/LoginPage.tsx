0ba5ccf4192ac2ebbad4faabe873b6554e129d17
mohamedYahiaIb <ibriz.yahia@univ-constantine2.dz>
2026-09-11 12:33:14 +0100

    finishing the login page and moving on to the register page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,8 +5,10 @@ function LoginPage() {
     return (
         <div className="w-full flex flex-row-reverse">
             <div className="w-full lg:w-1/2 flex flex-col px-6 pt-50">
-                <p className="text-white font-medium font-sans text-4xl mb-2 pl-34">WELCOME BACK</p>
-                <p className="text-gray-500 font-medium font-sans mb-5 pl-34">Sign in to your FitCore Pro</p>
+                <div className="w-110 mx-auto">
+                    <p className="text-white font-medium font-sans text-4xl mb-2">WELCOME BACK</p>
+                    <p className="text-gray-500 font-medium font-sans mb-5 ">Sign in to your FitCore Pro</p>
+                </div>
                 <LoginForm />
             </div>
             <div className="w-1/2 hidden lg:flex lg:w-1/2">
